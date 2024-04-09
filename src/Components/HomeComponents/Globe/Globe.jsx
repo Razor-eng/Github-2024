ba06faa4f40b1acd0916f6b7627b8ec88ec817fa
Razor-eng <rajatmaharana64@gmail.com>
2024-04-09 19:45:33 +0530

    first commit

--- src/Components/HomeComponents/Globe/Globe.jsx
+++ src/Components/HomeComponents/Globe/Globe.jsx
@@ -25,7 +25,7 @@ const Globe = () => {
                     </div>
                 </div>
             </div>
-            <div className={`relative z-[2] max-sm:hidden bottom-16 left-20`}>
+            <div className={`relative z-[2] max-sm:hidden bottom-0 left-20`}>
                 <Earth1 />
             </div>
             <img className="absolute bottom-0 left-0 w-full h-full object-cover d-block pointer-events-none" width="801" height="807" loading="lazy" decoding="async" alt="" aria-hidden="true" src="/images/home/globe/footer-galaxy.jpg" />
